1f897bfbd1122b985d4ae3c36faba164bc2be3bd
mazen87 <mazen.karfoul87@hotmail.com>
2021-08-22 04:17:43 +0200

    init application

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+      <img alt="Vue logo" src="../assets/welcome-1.jpg">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
